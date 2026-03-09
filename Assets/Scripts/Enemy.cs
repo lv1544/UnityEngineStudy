411c8cee9c2852b08be2a25217d8ba385a654436
57993233+lv1544@users.noreply.github.com <57993233+lv1544@users.noreply.github.com>
2026-03-10 00:16:11 +0900

    에너미 추가

---
+++ Assets/Scripts/Enemy.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public enum State 
+    {
+        None = -1, // 사용전
+        Ready = 0, // 준비완료
+        Appear,    //등장
+        Battle,    //전투중
+        Dead,      //사망
+        Disappear, //퇴장
+    }
+    
+    [SerializeField]
+    State CurrentState = State.None;
+
+    private const float MaxSpeed = 10.0f;
+    private const float MaxSpeedTime = 10.0f;
+    
+    [SerializeField]
+    Vector3 TargetPosition = Vector3.zero;
+
+    [SerializeField] private float CurrentSpeed; 
+    
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
