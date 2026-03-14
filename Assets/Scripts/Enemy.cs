971f733e1d23510518b531ac1cbd58bf51bbdbb7
57993233+lv1544@users.noreply.github.com <57993233+lv1544@users.noreply.github.com>
2026-03-14 12:49:57 +0900

    적 상태추가 시간에 따라 이동하고 사라짐

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour
@@ -23,6 +24,11 @@ public class Enemy : MonoBehaviour
 
     [SerializeField] private float CurrentSpeed; 
     
+    Vector3 CurrentVelocity;
+
+    float MoveStartTime = 0.0f;
+    float BattleStartTime = 0.0f;
+    
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
@@ -32,39 +38,97 @@ public class Enemy : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.L))
+        {
+            Appear(new Vector3(7.0f,transform.position.y,transform.position.z));
+        }
+
+        switch (CurrentState)
+        {
+            case State.None:
+            case State.Ready:
+                break;
+            case State.Dead:
+                break;
+            case State.Appear:
+            case State.Disappear:
+                UpdateSpeed();
+                UpdateMove(); 
+                break;
+            case State.Battle:
+                UpdateBattle();
+                break;
+        }
+
     }
 
     void UpdateSpeed()
     {
-        
+       CurrentSpeed = Mathf.Lerp(CurrentSpeed, MaxSpeed,(Time.time - MoveStartTime)/MaxSpeedTime );
     }
 
     void UpdateMove()
     {
+        float distance = Vector3.Distance(TargetPosition, transform.position);
+        
+        if (distance <= 0.01f)
+        {
+            Arrived();
+        }
+        
+        if (CurrentSpeed <= 0.01f)
+        {
+            return;
+        }
+        
+        CurrentVelocity = (TargetPosition - transform.position).normalized * CurrentSpeed;
+        
+       transform.position = Vector3.SmoothDamp(transform.position,TargetPosition, ref CurrentVelocity,distance/ CurrentSpeed,MaxSpeed);
         
     }
 
     void Arrived()
     {
+        CurrentSpeed = 0.0f;
         
-    }
+        if (CurrentState == State.Appear)
+        {
+            CurrentState = State.Battle;
+            BattleStartTime = Time.time;
+        }
+        else // if( currentState == State.Disappear)
+        {
+            CurrentState = State.None;
+        }
 
-    public void Apear(Vector3 targetPos)
+}
+
+    public void Appear(Vector3 targetPos)
     {
         TargetPosition = targetPos;
         CurrentSpeed = MaxSpeed;
         CurrentState = State.Appear;
+        MoveStartTime = Time.time;
     }
 
     public void Disappear(Vector3 targetPos)
     {
         TargetPosition = targetPos;
-        CurrentSpeed = MaxSpeed; 
+        CurrentSpeed = 0.0f;
         CurrentState = State.Disappear;
+        MoveStartTime = Time.time;
 
     }
     
     
+    void UpdateBattle()
+    {
+        if (Time.time - BattleStartTime > 3.0f)
+        {
+            Disappear(new Vector3(-15.0f,transform.position.y,transform.position.z));
+        }
+   
+    }
+
     
 }
